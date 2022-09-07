6568d7dbb120333ddc5d0dd79991cdb5ee697dfc
TT1410 <112493080+TT1410@users.noreply.github.com>
2022-09-07 15:30:48 +0300

    Update register_nadlers.py

--- bot/register_nadlers.py
+++ bot/register_nadlers.py
@@ -3,13 +3,13 @@ from .handlers import User
 DICT_FUNC = {}
 
 
-def register_message_handler(func, commands: str | list, arguments: User = None):
+def register_message_handler(func, commands: str | list, arguments: User = None, quantity_arg: int = 0):
     if isinstance(commands, str):
         commands = [commands]
 
     for cmd in commands:
         if arguments:
-            DICT_FUNC.update({cmd: {'function': func, "arguments": arguments}})
+            DICT_FUNC.update({cmd: {'function': func, "arguments": arguments, "quantity_arg": quantity_arg}})
         else:
             DICT_FUNC[cmd] = func
 
