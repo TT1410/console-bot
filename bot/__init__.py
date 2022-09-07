eba4e5ed327a68eeb17d6fd5d52f7086f7cfcf05
TT1410 <112493080+TT1410@users.noreply.github.com>
2022-09-07 15:30:12 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,4 @@
-from .handlers import User, UserPhone, hello, add_user, change_user, user_phone, show_all_users, close_bot
+from .handlers import User, hello, add_user, change_user, user_phone, show_all_users, close_bot
 from .register_nadlers import DICT_FUNC, register_message_handler
 from .error import input_error
 
@@ -11,8 +11,8 @@ __all__ = [
 
 
 register_message_handler(hello, 'hello')
-register_message_handler(add_user, 'add', User)
-register_message_handler(change_user, 'change', User)
-register_message_handler(user_phone, 'phone', UserPhone)
+register_message_handler(add_user, 'add', User, 2)
+register_message_handler(change_user, 'change', User, 2)
+register_message_handler(user_phone, 'phone', User, 1)
 register_message_handler(show_all_users, 'show all')
 register_message_handler(close_bot, ["good bye", "close", "exit"])
