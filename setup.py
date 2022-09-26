c765a4b56c1ebb042a79606e1e968defa3ecbb53
TT1410 <112493080+TT1410@users.noreply.github.com>
2022-09-26 23:26:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='console_bot',
-    version='1',
+    version='1.0.0',
     description='Консольний бот',
-    url='https://github.com/TT1410/sort_folder',
+    url='https://github.com/TT1410/console-bot',
     author='TT1410',
-    author_email='tarplax@gmail.com',
+    author_email='test@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
     include_package_data=True,
